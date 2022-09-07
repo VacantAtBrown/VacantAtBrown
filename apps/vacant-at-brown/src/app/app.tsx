1e7eb417e7908314c3b80624467ff10c18556f51
Hammad Izhar <hammad_izhar@brown.edu>
2022-09-07 17:10:53 -0400

    tailwind broken :(

--- apps/vacant-at-brown/src/app/app.tsx
+++ apps/vacant-at-brown/src/app/app.tsx
@@ -7,28 +7,7 @@ export interface Message {
 }
 
 export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
-
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
-
-  return (
-    <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to vacant-at-brown!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          alt="Nx - Smart, Fast and Extensible Build System"
-        />
-      </div>
-      <div>{m.message}</div>
-      <MapView />
-    </>
-  );
-};
+  return <MapView />
+}
 
 export default App;
--- apps/vacant-at-brown/src/components/Map.tsx
+++ apps/vacant-at-brown/src/components/Map.tsx
@@ -2,7 +2,7 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const MapView = () => {
     return (
-        <div>
+        <div className="h-full">
             <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
