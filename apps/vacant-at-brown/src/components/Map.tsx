57b17484ac5e9acaa0ce4c9ffe3dd629702f54a3
Hammad <hammad_izhar@brown.edu>
2022-09-08 08:29:13 -0400

    Add overlays for major buildings on campus

--- apps/vacant-at-brown/src/components/Map.tsx
+++ apps/vacant-at-brown/src/components/Map.tsx
@@ -1,34 +1,72 @@
-import { LatLng } from "leaflet";
-import { useState } from "react";
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import { latLng, LatLng } from 'leaflet';
+import { useEffect, useState } from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  useMapEvents,
+  Polygon,
+} from 'react-leaflet';
+import places from '../assets/locations.json';
 
 const Markers = () => {
-    const [markers, setMarkers] = useState<LatLng[]>([]);
+  const [markers, setMarkers] = useState<LatLng[]>([]);
 
-    const map = useMapEvents({
-        click: (location) => {
-            const {latlng} = location
-            console.log(markers)
-            return setMarkers([...markers, latlng])
-        },
-      })
+  useMapEvents({
+    click: (location) => {
+      const { latlng } = location;
+      console.log(markers);
+      return setMarkers([...markers, latlng]);
+    },
+  });
 
-    return <>
-        {markers.map((latlng, i) => <Marker position={latlng} key={i}/>)}
-        </>
-}
+  return (
+    <>
+      {markers.map((latlng, i) => (
+        <Marker position={latlng} key={i} />
+      ))}
+    </>
+  );
+};
+
+const Locations = () => {
+  const [shapes, setShapes] = useState<LatLng[][]>([]);
+
+  useEffect(() => {
+    for (const place in places) {
+      const vertices = places[place as keyof typeof places].map((obj) =>
+        latLng(obj)
+      );
+      setShapes((prevShapes) => [...prevShapes, vertices]);
+    }
+  }, []);
+
+  return (
+    <>
+      {shapes.map((latlng, i) => (
+        <Polygon positions={latlng} key={i} pathOptions={{ color: 'red' }} />
+      ))}
+    </>
+  );
+};
 
 const MapView = () => {
-
-    return (
-            <MapContainer className="h-screen" center={[41.82611606734716, -71.4031504947082]} zoom={30} scrollWheelZoom={false}>
-                <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Markers />
-            </MapContainer>
-    );
-}
+  return (
+    <MapContainer
+      className="h-screen"
+      center={[41.82611606734716, -71.4031504947082]}
+      zoom={30}
+      scrollWheelZoom={false}
+      zoomControl={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Locations />
+      {/* <Markers /> */}
+    </MapContainer>
+  );
+};
 
 export default MapView;
